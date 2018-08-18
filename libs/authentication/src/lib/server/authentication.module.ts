aee1ef8bd3ef42a305f8af86ee63eaecb4690468
john5117 <abiodunogundijo@gmail.com>
2018-08-18 09:00:13 +0100

    added passport authentication middleware for the application

--- libs/authentication/src/lib/server/authentication.module.ts
+++ libs/authentication/src/lib/server/authentication.module.ts
@@ -1,11 +1,21 @@
-import { AuthController } from '@dilta/authentication/src/lib/server/auth.controller';
-import { ClientAuthService } from '@dilta/authentication/src/lib/server/auth.service';
 import { DatabaseModule } from '@dilta/database/src';
 import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, NestModule } from '@nestjs/common/interfaces';
+import { AuthController } from './auth.controller';
+import { ClientAuthService } from './auth.service';
+import { AUTH_API_ROUTE } from './constants';
+import { PassportMiddleWareService } from './passport';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [ClientAuthService],
-  controllers: [AuthController]
+  providers: [ClientAuthService, PassportMiddleWareService],
+  controllers: [AuthController],
+  exports: [PassportMiddleWareService]
 })
-export class ServerAuthenticationModule {}
+export class ServerAuthenticationModule implements NestModule {
+  constructor(private pms: PassportMiddleWareService) {}
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(this.pms.middleWares).exclude(AUTH_API_ROUTE);
+  }
+}
--- libs/authentication/src/lib/server/constants.ts
+++ libs/authentication/src/lib/server/constants.ts
@@ -2,3 +2,4 @@ export const JWT_ALGORITHM = process.env.JWT_ALGORITHM;
 export const AUDIENCE = process.env.AUDIENCE;
 export const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY;
 export const BCRYPT_HASH_ROUND = 8;
+export const AUTH_API_ROUTE = '/auth';
